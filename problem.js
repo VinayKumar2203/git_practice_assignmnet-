67a7a5b248a23b435cf3c98ed36ae45edf447585
Naveen Kumar <nk24528.nk81@gmail.com>
2023-10-20 16:31:11 +0530

    this is dynamic approach to check any number is prime or not

--- problem.js
+++ problem.js
@@ -1,13 +1,19 @@
-let num =17;
-let count =0;
-for(let i=1;i<=num;i++){
-if(num%i==0){
-    count++
+
+function checkPrime(num)
+{
+    let count =0;
+    for(let i=1;i<=num;i++){
+    if(num%i==0){
+        count++
+    }
+    }
+    
+    if(count==2){
+        console.log("prime")
+    }
+    else{
+        console.log("not prime") 
+    }
 }
-}
-if(count==1){
-    console.log("prime")
-}
-else{
-    console.log("not prime") 
-}
+
+checkPrime(15)
